64d29ae4143fabfa56054634968842c717befa54
Kenechi Nzewi <caseynzewi@gmail.com>
2026-05-08 09:39:44 +0100

    feat(services): integrate genderize api service

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -3,17 +3,17 @@ from typing import Any
 
 
 def process_genderize_response(data: dict[str, Any]) -> dict[str, Any]:
-    name = data.get("name")
-    gender = data.get("gender")
-    probability = data.get("probability", 0)
-    sample_size = data.get("count", 0)
+    name: str = data.get("name", "")
+    gender: str = data.get("gender", "")
+    gender_probability: float = data.get("probability", 0)
+    sample_size: int = data.get("count", 0)
 
-    is_confident = (probability >= 0.7) and (sample_size >=100)
+    is_confident: bool = (gender_probability >= 0.7) and (sample_size >=100)
 
     return {
-        "name": name,
+        "name": name.lower(),
         "gender": gender,
-        "probability": probability,
+        "gender_probability": gender_probability,
         "sample_size": sample_size,
         "is_confident": is_confident,
         "processed_at": datetime.now(timezone.utc)
