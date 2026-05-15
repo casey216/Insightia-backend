3f68694ecfccbed061c2c37be7ec093e1565084f
Kenechi Nzewi <caseynzewi@gmail.com>
2026-05-15 09:42:03 +0100

    fix(settings): fix BASE_DIR path

--- src/api/core/settings.py
+++ src/api/core/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 
 class Settings(BaseSettings):
